cab32abe1914f8a729b22aa6c14192a337db10e0
maixhashi <maixhashi@gmail.com>
2024-07-30 06:59:45 +0900

    Feature_2024.07.30.06.58_カレンダーの非同期化実装（Ajax）

--- app/controllers/shuffled_overviews_controller.rb
+++ app/controllers/shuffled_overviews_controller.rb
@@ -2,9 +2,14 @@ class ShuffledOverviewsController < ApplicationController
   before_action :set_user
 
   def index
+    @start_date = params.fetch(:start_date, Date.today).to_date
     @shuffled_overviews = current_user.shuffled_overviews
     @grouped_overviews = current_user.shuffled_overviews.group_by_day(:created_at).count
     render 'users/shuffled_overviews/index'
+    respond_to do |format|
+      format.html # index.html.erb
+      format.js   # index.js.erb
+    end
   end
 
   def create
