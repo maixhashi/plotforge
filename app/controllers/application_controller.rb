27e7da129d3cd97c85cd1dca7ff1e30187a0fce3
maixhashi <maixhashi@gmail.com>
2024-08-06 05:02:15 +0900

    Feature_2024.08.05.07.16_コントローラーの設定 #26

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :require_login
+    helper_method :user_signed_in?, :current_user
   
     private
   
@@ -12,6 +13,19 @@ class ApplicationController < ActionController::Base
     def logged_in?
       !!current_user
     end
+
+    
+    # ユーザーがサインインしているかどうかを判定
+    def user_signed_in?
+      current_user.present?
+    end
+  
+    # 現在のユーザーを取得
+    def current_user
+      # 実際の実装に応じて変更してください
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    
   
   end
   
