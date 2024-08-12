cc51261ed29f445242b59e49cb290c28f0f6dc2c
maixhashi <maixhashi@gmail.com>
2024-08-12 14:43:51 +0900

    Feature_2024.08.11.22.16_ブックマークしたShuffledOverviewモデルレコードを表示・作成日ソートする機能を実装する_#36

--- app/controllers/bookmark_of_shuffled_overviews_controller.rb
+++ app/controllers/bookmark_of_shuffled_overviews_controller.rb
@@ -3,31 +3,42 @@ class BookmarkOfShuffledOverviewsController < ApplicationController
   def index
     @bookmarked_shuffled_overviews = current_user.bookmarked_shuffled_overviews
 
+    @grouped_bookmark_for_count = BookmarkOfShuffledOverview
+    .where(user_id: current_user.id)
+    .select("DATE(created_at) AS date, COUNT(*) AS count")
+    .group("DATE(created_at)")
+    .map { |record| [record.date.to_date, record.count] }
+    .to_h
+
+    @grouped_bookmark_for_count.inspect
+
     @shuffled_overviews = current_user.shuffled_overviews
 
     # 映画データを取得する
     tmdb_service = TmdbService.new
     @movies_data = {}
-    @shuffled_overviews.each do |shuffled_overview|
-      shuffled_overview.movie_ids.each do |movie_id|
-        @movies_data[movie_id] ||= tmdb_service.fetch_movie_details(movie_id)
-      end
-    end
+    date_param = params[:date].presence || Date.today.to_s
 
-    # MySQL で日付ごとにカウントを取得
+    begin
+      date = date_param.to_date
+    rescue ArgumentError
+      date = Date.today
+    end
+    
+    @date_range = date.beginning_of_day..date.end_of_day
+
     @grouped_bookmarked_shuffled_overviews = current_user.bookmarked_shuffled_overviews
-    .select('shuffled_overviews.id, shuffled_overviews.content, DATE(bookmark_of_shuffled_overviews.created_at) AS date, COUNT(*) AS count')
+    .where(bookmark_of_shuffled_overviews: { created_at: @date_range })
+    .select('shuffled_overviews.id, shuffled_overviews.content, shuffled_overviews.movie_ids, DATE(bookmark_of_shuffled_overviews.created_at) AS date, COUNT(*) AS count')
     .joins(:bookmark_of_shuffled_overviews)
-    .group('shuffled_overviews.id, shuffled_overviews.content, DATE(bookmark_of_shuffled_overviews.created_at)')
+    .group('shuffled_overviews.id, shuffled_overviews.content, shuffled_overviews.movie_ids, DATE(bookmark_of_shuffled_overviews.created_at)')
     .each_with_object({}) do |overview, hash|
       date = overview.date
       hash[date] ||= []
       hash[date] << overview
     end
 
-    date_key = @grouped_bookmarked_shuffled_overviews.keys.first
-    Rails.logger.debug "ShuffledOverview movie_ids: #{@grouped_bookmarked_overviews.inspect}"
-                               
+    @grouped_bookmarked_shuffled_overviews.inspect
 
     render 'users/bookmark_of_shuffled_overviews/index'
   end
@@ -55,15 +66,25 @@ class BookmarkOfShuffledOverviewsController < ApplicationController
   end
   
   def filter_bookmarked_overviews_by_date
+    @grouped_bookmark_for_count = BookmarkOfShuffledOverview
+    .where(user_id: current_user.id)
+    .select("DATE(created_at) AS date, COUNT(*) AS count")
+    .group("DATE(created_at)")
+    .map { |record| [record.date.to_date, record.count] }
+    .to_h
+
+    @grouped_bookmark_for_count.inspect
+
     # 日付パラメータが存在しない場合は Date.today を使用
-    date_param = params[:date].presence || Date.today.to_s
+    Time.zone = 'UTC'
+    date_param = params[:date].presence || Date.current.to_s
     
     begin
       date = date_param.to_date
     rescue ArgumentError
-      date = Date.today
+      date = Date.current
     end
-    
+
     @bookmarked_shuffled_overviews = current_user.bookmarked_shuffled_overviews
     
     # 指定された日付範囲のデータを取得
@@ -72,23 +93,29 @@ class BookmarkOfShuffledOverviewsController < ApplicationController
     # 映画データを再取得する
     tmdb_service = TmdbService.new
     @movies_data = {}
-    @shuffled_overviews.each do |shuffled_overview|
-      shuffled_overview.movie_ids.each do |movie_id|
+    @bookmarked_shuffled_overviews.each do |bookmarked_shuffled_overview|
+      bookmarked_shuffled_overview.movie_ids.each do |movie_id|
         @movies_data[movie_id] ||= tmdb_service.fetch_movie_details(movie_id)
       end
     end
     
-    # MySQL で日付ごとにカウントを取得
+    @movies_data.inspect
+    @movies_data['poster_path'].inspect
+    
+    @date_range = date.beginning_of_day..date.end_of_day
+
     @grouped_bookmarked_shuffled_overviews = current_user.bookmarked_shuffled_overviews
-    .where(created_at: date.all_day)
-    .select('shuffled_overviews.id, shuffled_overviews.content, DATE(bookmark_of_shuffled_overviews.created_at) AS date, COUNT(*) AS count')
+    .where(bookmark_of_shuffled_overviews: { created_at: @date_range })
+    .select('shuffled_overviews.id, shuffled_overviews.content, shuffled_overviews.movie_ids, DATE(bookmark_of_shuffled_overviews.created_at) AS date, COUNT(*) AS count')
     .joins(:bookmark_of_shuffled_overviews)
-    .group('shuffled_overviews.id, shuffled_overviews.content, DATE(bookmark_of_shuffled_overviews.created_at)')
+    .group('shuffled_overviews.id, shuffled_overviews.content, shuffled_overviews.movie_ids, DATE(bookmark_of_shuffled_overviews.created_at)')
     .each_with_object({}) do |overview, hash|
       date = overview.date
       hash[date] ||= []
       hash[date] << overview
     end
+
+    @grouped_bookmarked_shuffled_overviews.inspect
     
     respond_to do |format|
       format.html { render 'users/bookmark_of_shuffled_overviews/index' }
