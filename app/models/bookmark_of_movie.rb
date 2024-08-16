cb7d24c917f4df6c5c7cccd8aef538796208b843
maixhashi <maixhashi@gmail.com>
2024-08-17 04:37:57 +0900

    Feature_2024.08.17.04.37_associationの設定

--- app/models/bookmark_of_movie.rb
+++ app/models/bookmark_of_movie.rb
@@ -1,4 +1,6 @@
 class BookmarkOfMovie < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+
+  validates :user_id, uniqueness: { scope: :movie_id }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  
   validates :email, uniqueness: true
+  
   has_many :shuffled_overviews, dependent: :destroy
   has_many :bookmark_of_shuffled_overviews
   has_many :bookmarked_shuffled_overviews, through: :bookmark_of_shuffled_overviews, source: :shuffled_overview
+  has_many :bookmark_of_movies, dependent: :destroy
+  has_many :bookmarked_movies, through: :bookmark_of_movies, source: :movie
 end
