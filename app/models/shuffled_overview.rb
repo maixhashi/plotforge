57835df5215f472154e5f18fbd8f1c388196fad8
maixhashi <maixhashi@gmail.com>
2024-08-16 21:18:08 +0900

    Feature_2024.08.16.21.17_ShuffledOverview-LinkOfShuffledOverviewMovie-Movieのassociationの実装

--- app/models/shuffled_overview.rb
+++ app/models/shuffled_overview.rb
@@ -1,6 +1,10 @@
 class ShuffledOverview < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
-  has_many :bookmark_of_shuffled_overviews
+  has_many :bookmark_of_shuffled_overviews, dependent: :destroy
   has_many :users, through: :bookmark_of_shuffled_overviews
+  has_many :link_of_shuffled_overview_movies
+  has_many :movies, through: :link_of_shuffled_overview_movies
+  accepts_nested_attributes_for :link_of_shuffled_overview_movies
 end
+
