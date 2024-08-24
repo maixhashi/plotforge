371b57e89d4dd4f08d4cb39d6ba66c55bfea2962
maixhashi <maixhashi@gmail.com>
2024-08-25 01:08:49 +0900

    Feature_2024.08.25.01.08_associationの実装

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,3 +1,6 @@
 class Keyword < ApplicationRecord
+  has_many :shuffled_overview_keywords
+  has_many :shuffled_overviews, through: :shuffled_overview_keywords
+  
   validates :content, presence: true, uniqueness: true
 end
