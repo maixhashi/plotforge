762441556ee3211053318fe482f6cc7535a17a6e
maixhashi <maixhashi@gmail.com>
2024-09-05 05:46:00 +0900

    Feature_2024.09.05.05.45_ジャンルソート用のフォームを追加

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -7,8 +7,19 @@ class TimelineController < ApplicationController
 
     # 取得したユーザーIDに基づいて、該当ユーザーのShuffledOverviewを取得
     @shuffled_overviews = ShuffledOverview.where(user_id: user_ids).order(created_at: :desc)
-    @shuffled_overviews_on_timeline = @shuffled_overviews.page(params[:shuffled_overviews_page]).per(5)
 
+    # ジャンルフィルタリングの適用
+    if params[:genre_id].present?
+      genre_id = params[:genre_id].to_i
+      @shuffled_overviews = @shuffled_overviews.select do |shuffled_overview|
+        shuffled_overview.related_movie_ids.any? do |movie_id|
+          movie_details = TmdbService.new.fetch_movie_details(movie_id)
+          movie_details['genres'].any? { |genre| genre['id'] == genre_id }
+        end
+      end
+    end
+
+    # TMDBから映画の詳細情報を取得
     tmdb_service = TmdbService.new
     @movies_data = {}
     @shuffled_overviews.each do |shuffled_overview|
@@ -16,32 +27,24 @@ class TimelineController < ApplicationController
         @movies_data[movie_id] ||= tmdb_service.fetch_movie_details(movie_id)
       end
     end
-  end
 
-  def update_overviews
-    case params[:filter]
-    when 'my-overviews'
-      @shuffled_overviews_on_timeline = current_user.shuffled_overviews
-    when 'following-user-overviews'
-      @shuffled_overviews_on_timeline = ShuffledOverview.where(user: current_user.following)
-    when 'both-overviews'
-      @shuffled_overviews_on_timeline = ShuffledOverview.where(user: [current_user, *current_user.following])
-    else
-      @shuffled_overviews_on_timeline = []
-    end
-    
-    tmdb_service = TmdbService.new
-    @movies_data = {}
-    @shuffled_overviews_on_timeline.each do |shuffled_overview|
+    # ページネーションの適用
+    @shuffled_overviews_on_timeline = Kaminari.paginate_array(@shuffled_overviews).page(params[:shuffled_overviews_page]).per(5)
+
+    # 全てのジャンルデータの取得
+    @genres_data = {}
+    all_genres = []
+    @shuffled_overviews.each do |shuffled_overview|
       shuffled_overview.related_movie_ids.each do |movie_id|
-        @movies_data[movie_id] ||= tmdb_service.fetch_movie_details(movie_id)
+        movie_details = tmdb_service.fetch_movie_details(movie_id)
+        movie_details['genres'].each do |genre|
+          all_genres << genre
+        end
       end
     end
 
-    respond_to do |format|
-      format.js
-    end
-    render partial: 'users/shuffled_overviews/shuffled_overview_list_on_timeline', locals: { shuffled_overviews_on_timeline: @shuffled_overviews_on_timeline }
+    # 重複を排除してジャンルデータを設定
+    @genres_data = all_genres.uniq { |genre| genre['id'] }.map { |genre| [genre['id'], genre['name']] }.to_h
   end
 
   private
@@ -49,5 +52,4 @@ class TimelineController < ApplicationController
   def set_user
     @user = current_user
   end
-
 end
