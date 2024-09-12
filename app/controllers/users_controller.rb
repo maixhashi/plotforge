118fdd66cd96e3938d3774f0dea76af509f289b0
maixhashi <maixhashi@gmail.com>
2024-09-13 05:32:18 +0900

    Feature_2024.09.13.05.31_contribution calendarの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,88 +32,59 @@ class UsersController < ApplicationController
 
   def mypage
     @bookmarked_shuffled_overviews = current_user.bookmarked_shuffled_overviews
-
     @shuffled_overviews = current_user.shuffled_overviews
     @shuffled_overviews_on_profile = current_user.shuffled_overviews.page(params[:shuffled_overviews_page]).per(5)
-
+  
     # Bookmarked ShuffledOverviewsのIDを取得
     bookmarked_shuffled_overview_ids = current_user.bookmarked_shuffled_overviews.pluck(:shuffled_overview_id)
-
+    
     # そのIDに基づいてShuffledOverviewを取得し、ページネーションを適用
     @bookmarked_shuffled_overviews_on_profile = ShuffledOverview.where(id: bookmarked_shuffled_overview_ids).page(params[:bookmarked_shuffled_overviews_page]).per(5)
-
+  
     tmdb_service = TmdbService.new
     @movies_data = {}
     @movies_data_related_of_bookmarked_overviews = {}
-    # @shuffled_overviews.each do |shuffled_overview|
-    #   shuffled_overview.related_movie_ids.each do |movie_id|
-    #     @movies_data[movie_id] ||= tmdb_service.fetch_movie_details(movie_id)
-    #   end
-    # end
-
+  
     # ユニークな movie_id を取得する
     unique_movie_ids = @shuffled_overviews.flat_map(&:related_movie_ids).uniq
-
+  
     # ユニークな movie_id を使って映画の詳細をフェッチ
     unique_movie_ids.each do |movie_id|
       @movies_data[movie_id] ||= tmdb_service.fetch_movie_details(movie_id)
     end
     @movies_array = @movies_data.values
     @paginated_movies = Kaminari.paginate_array(@movies_array).page(params[:movies_page]).per(12)
-    
-    
+  
     @bookmarked_shuffled_overviews_on_profile.each do |shuffled_overview|
       shuffled_overview.related_movie_ids.each do |movie_id|
         @movies_data_related_of_bookmarked_overviews[movie_id] ||= tmdb_service.fetch_movie_details(movie_id)
       end
     end
-    
-    
-    # <%= render partial: 'users/bookmark_of_shuffled_overviews/bookmarked_shuffled_overview_list_on_profile', locals: { bookmarked_shuffled_overviews: @msy_bookmarked_shuffled_overviews } %>で渡す変数
-    results = current_user.bookmarked_shuffled_overviews
-      .select('shuffled_overviews.id, shuffled_overviews.content, shuffled_overviews.related_movie_ids, DATE(bookmark_of_shuffled_overviews.created_at) AS date, COUNT(*) AS count')
-      .joins(:bookmark_of_shuffled_overviews)
-      .group('shuffled_overviews.id, shuffled_overviews.content, shuffled_overviews.related_movie_ids, DATE(bookmark_of_shuffled_overviews.created_at)')
-      .order('date DESC')
-      .limit(4) # ここでレコードを5つに制限
   
-    # 結果をハッシュに変換
-    @grouped_bookmarked_shuffled_overviews = results.each_with_object({}) do |overview, hash|
-      date = overview.date
-      hash[date] ||= []
-      hash[date] << overview
+    # Contributionカレンダーのためのデータを準備
+    # `created_at`をキーとして、作成日ごとにShuffledOverviewをカウント
+    @shuffled_overviews_by_date = current_user.shuffled_overviews.group_by { |overview| overview.created_at.to_date }
+  
+    # 日ごとのカウントを保持するハッシュ
+    @shuffled_overviews_count_by_date = @shuffled_overviews_by_date.each_with_object({}) do |(date, overviews), hash|
+      hash[date] = overviews.size
     end
-
-    @grouped_bookmarked_movies = current_user.bookmarked_movies
-    .select('DATE(bookmark_of_movies.created_at) AS date, movies.tmdb_id, COUNT(*) AS count')
-    .joins(:bookmark_of_movies)
-    .group('DATE(bookmark_of_movies.created_at), movies.tmdb_id')
-
-
-    filtered_bookmarked_movies = current_user.bookmarked_movies
-    .select('DATE(bookmark_of_movies.created_at) AS date, movies.tmdb_id')
-    .joins(:bookmark_of_movies)
-    .group('DATE(bookmark_of_movies.created_at), movies.tmdb_id')
-    .limit(4)
-
-    # 常に配列を値として持つハッシュを初期化
+  
+    # ブックマークされた映画に関連する処理
     @bookmarked_movies_by_date = Hash.new { |hash, key| hash[key] = [] }
-
-    filtered_bookmarked_movies.each do |movie|
-      date = movie.date
+    current_user.bookmarked_movies.each do |movie|
+      date = movie.created_at.to_date
       tmdb_id = movie.tmdb_id
       @bookmarked_movies_by_date[date] << tmdb_id
     end
-
+  
     @bookmarked_movies_data = {}
-
     current_user.bookmarked_movies.each do |movie|
       tmdb_id = movie.tmdb_id
       @bookmarked_movies_data[tmdb_id] ||= tmdb_service.fetch_movie_details(tmdb_id)
     end
-
   end
-
+  
   def movie_poster_path(tmdb_id)
     # TMDB から映画の詳細を取得するメソッドがあると仮定
     tmdb_service = TmdbService.new
