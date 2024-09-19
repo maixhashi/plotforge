2f4900797ca233534ec399df91ea23d054b4a555
maixhashi <maixhashi@gmail.com>
2024-09-20 01:45:21 +0900

    Test_2024.09.20.01.44_validationの追加 / LinkOfShuffledOverviewMovieモデルのモデルスペックの追加(validation)

--- app/models/link_of_shuffled_overview_movie.rb
+++ app/models/link_of_shuffled_overview_movie.rb
@@ -1,4 +1,7 @@
 class LinkOfShuffledOverviewMovie < ApplicationRecord
+  ## Validation 
+  validates :shuffled_overview_id, uniqueness: { scope: :movie_id }
+
   ## Association
   belongs_to :shuffled_overview
   belongs_to :movie
--- spec/models/link_of_shuffled_overview_movie_spec.rb
+++ spec/models/link_of_shuffled_overview_movie_spec.rb
@@ -10,6 +10,14 @@ RSpec.describe LinkOfShuffledOverviewMovie, type: :model do
       expect(link).to be_valid
     end
 
+    it 'does not allow the same shuffled_overview and movie combination more than once' do
+      LinkOfShuffledOverviewMovie.create(shuffled_overview: shuffled_overview, movie: movie)
+      duplicate_link = LinkOfShuffledOverviewMovie.new(shuffled_overview: shuffled_overview, movie: movie)
+
+      expect(duplicate_link).not_to be_valid
+    end
+  end
+
   describe 'Associations' do
     it { should belong_to(:shuffled_overview) }
     it { should belong_to(:movie) }
