60bbeb45a6b89c670a3d6676dc78c44ca69328d7
maixhashi <maixhashi@gmail.com>
2024-09-20 05:00:33 +0900

    Test_2024.09.20.05.00_Characterモデルのモデルスペックテストを追加

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,5 @@
 class Character < ApplicationRecord
+  ## Association
   has_many :appearance_of_character, dependent: :destroy
   has_many :shuffled_overviews, through: :appearance_of_character
 end
---
+++ spec/models/character_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Character, type: :model do
+  # モデルが存在することをテスト
+  it 'is valid with valid attributes' do
+    character = Character.new(name: 'John Doe')
+    expect(character).to be_valid
+  end
+
+  # 関連付けのテスト
+  describe 'associations' do
+    it 'has many appearance_of_characters' do
+      association = described_class.reflect_on_association(:appearance_of_character)
+      expect(association.macro).to eq :has_many
+    end
+
+    it 'has many shuffled_overviews through appearance_of_characters' do
+      association = described_class.reflect_on_association(:shuffled_overviews)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :appearance_of_character
+    end
+  end
+
+  # インデックスが正しく設定されているかをテスト
+  it 'has a name index' do
+    column = ActiveRecord::Base.connection.columns('characters').find { |c| c.name == 'name' }
+    expect(column).not_to be_nil
+    index = ActiveRecord::Base.connection.indexes('characters').find { |i| i.name == 'index_characters_on_name' }
+    expect(index).not_to be_nil
+  end
+end
