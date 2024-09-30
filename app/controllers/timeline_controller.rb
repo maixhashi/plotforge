a7e1cf6db2b6e0cb3cd0f2df9303eaf459a6aa6c
maixhashi <maixhashi@gmail.com>
2024-09-30 09:44:30 +0900

    Feature_2024.09.30.09.44_ゲストユーザーの場合のタイムライン表示レコードの条件分岐を実装

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,12 +2,23 @@ class TimelineController < ApplicationController
   before_action :set_user
 
   def index
-    # 現在のユーザーとフォロワーのユーザーIDを取得
-    user_ids = [current_user.id] + current_user.following.pluck(:id)
-
-    # 取得したユーザーIDに基づいて、該当ユーザーのShuffledOverviewを取得
-    @shuffled_overviews = ShuffledOverview.where(user_id: user_ids).order(created_at: :desc)
-
+    # 現在のユーザーがゲストユーザーかどうかを判定
+    guest_user = User.find_by(email: 'guest@example.com')
+  
+    if current_user == guest_user
+      # ゲストユーザーの場合、ゲストユーザー自身のShuffledOverviewのみ取得
+      @shuffled_overviews = ShuffledOverview.where(user_id: current_user.id).order(created_at: :desc)
+    else
+      # 通常ユーザーの場合、フォロワーと自身のShuffledOverviewを取得し、ゲストユーザーのレコードを除外
+      user_ids = [current_user.id] + current_user.following.pluck(:id)
+  
+      # ゲストユーザーのIDを除外
+      guest_user_id = guest_user&.id
+      @shuffled_overviews = ShuffledOverview.where(user_id: user_ids)
+                                            .where.not(user_id: guest_user_id)
+                                            .order(created_at: :desc)
+    end
+  
     # TMDBから映画の詳細情報を取得し、ユーザー情報を追加
     tmdb_service = TmdbService.new
     @movies_data = {}
@@ -19,18 +30,30 @@ class TimelineController < ApplicationController
         }
       end
     end
-
+  
     # ページネーションの適用
     @shuffled_overviews_on_timeline = Kaminari.paginate_array(@shuffled_overviews).page(params[:shuffled_overviews_page]).per(5)
   end
+  
 
   def timeline_shuffled_overviews
-    # 現在のユーザーとフォロワーのユーザーIDを取得
-    user_ids = [current_user.id] + current_user.following.pluck(:id)
+    # 現在のユーザーがゲストユーザーかどうかを判定
+    guest_user = User.find_by(email: 'guest@example.com')
 
-    # 取得したユーザーIDに基づいて、該当ユーザーのShuffledOverviewを取得
-    @shuffled_overviews = ShuffledOverview.where(user_id: user_ids).order(created_at: :desc)
-
+    if current_user == guest_user
+      # ゲストユーザーの場合、ゲストユーザー自身のShuffledOverviewのみ取得
+      @shuffled_overviews = ShuffledOverview.where(user_id: current_user.id).order(created_at: :desc)
+    else
+      # 通常ユーザーの場合、フォロワーと自身のShuffledOverviewを取得し、ゲストユーザーのレコードを除外
+      user_ids = [current_user.id] + current_user.following.pluck(:id)
+  
+      # ゲストユーザーのIDを除外
+      guest_user_id = guest_user&.id
+      @shuffled_overviews = ShuffledOverview.where(user_id: user_ids)
+                                            .where.not(user_id: guest_user_id)
+                                            .order(created_at: :desc)
+    end
+  
     # TMDBから映画の詳細情報を取得し、ユーザー情報を追加
     tmdb_service = TmdbService.new
     @movies_data = {}
@@ -42,18 +65,28 @@ class TimelineController < ApplicationController
         }
       end
     end
-
+  
     # ページネーションの適用
     @shuffled_overviews_on_timeline = Kaminari.paginate_array(@shuffled_overviews).page(params[:shuffled_overviews_page]).per(5)
   end
 
   def timeline_movies
-    # 現在のユーザーとフォロワーのユーザーIDを取得
-    user_ids = [current_user.id] + current_user.following.pluck(:id)
+    guest_user = User.find_by(email: 'guest@example.com')
 
-    # 取得したユーザーIDに基づいて、該当ユーザーのShuffledOverviewを取得
-    @shuffled_overviews = ShuffledOverview.where(user_id: user_ids).order(created_at: :desc)
-
+    if current_user == guest_user
+      # ゲストユーザーの場合、ゲストユーザー自身のShuffledOverviewのみ取得
+      @shuffled_overviews = ShuffledOverview.where(user_id: current_user.id).order(created_at: :desc)
+    else
+      # 通常ユーザーの場合、フォロワーと自身のShuffledOverviewを取得し、ゲストユーザーのレコードを除外
+      user_ids = [current_user.id] + current_user.following.pluck(:id)
+  
+      # ゲストユーザーのIDを除外
+      guest_user_id = guest_user&.id
+      @shuffled_overviews = ShuffledOverview.where(user_id: user_ids)
+                                            .where.not(user_id: guest_user_id)
+                                            .order(created_at: :desc)
+    end
+    
     # TMDBから映画の詳細情報を取得し、ユーザー情報を追加
     tmdb_service = TmdbService.new
     @movies_data = {}
